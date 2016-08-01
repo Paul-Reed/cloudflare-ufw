7aa340f269675ec06f844d45820b0f9efc802b61
Paul-Reed <pauldreed@gmail.com>
2016-08-01 21:10:22 +0100

    First commit

---
+++ cloudflare-ufw.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DIR="$(dirname $(readlink -f $0))"
+cd $DIR
+wget https://www.cloudflare.com/ips-v4 -O ips-v4.tmp
+wget https://www.cloudflare.com/ips-v6 -O ips-v6.tmp
+mv ips-v4.tmp ips-v4
+mv ips-v6.tmp ips-v6
+
+# Uncomment these 2 if you need port 80
+#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
+#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
+
+for cfip in `cat ips-v4`; do ufw allow from $cfip; done
+for cfip in `cat ips-v6`; do ufw allow from $cfip; done
+
+#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
+#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
