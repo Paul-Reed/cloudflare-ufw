96da8fdc9870fbbe49a70df859e375a133f72584
Paul Reed <pauldreed@gmail.com>
2016-08-01 21:24:24 +0100

    Update cloudflare-ufw.sh

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -7,12 +7,14 @@ wget https://www.cloudflare.com/ips-v6 -O ips-v6.tmp
 mv ips-v4.tmp ips-v4
 mv ips-v6.tmp ips-v6
 
-# Uncomment these 2 if you need port 80
-#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
-#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
-
 for cfip in `cat ips-v4`; do ufw allow from $cfip; done
 for cfip in `cat ips-v6`; do ufw allow from $cfip; done
 
+# OTHER EXAMPLE RULES
+# Uncomment these 2 if you need to retrict to port 80
+#for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 80 proto tcp; done
+#for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 80 proto tcp; done
+
+# Uncomment these 2 if you need to restrict to port 443
 #for cfip in `cat ips-v4`; do ufw allow from $cfip to any port 443 proto tcp; done
 #for cfip in `cat ips-v6`; do ufw allow from $cfip to any port 443 proto tcp; done
