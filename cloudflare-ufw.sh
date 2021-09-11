6586d5bf782603af7ccf2956bda07d4f444ab35a
JunkMeal <65283415+JunkMeal@users.noreply.github.com>
2021-09-11 21:08:00 +0200

    Bug
    Bug that caused the last v4 ip not to be added because there wasnt a newline

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
+echo "" >> /tmp/cf_ips
 curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 
 # Allow all traffic from Cloudflare IPs (no ports restriction)
