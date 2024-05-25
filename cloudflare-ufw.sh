de5ffc544eaf5e7d89bfc69cb0182c6d07cd0b59
Paul <pauldreed@gmail.com>
2024-05-25 21:01:07 +0100

    Allow udp protocol for port 443 example
    For compatibility with HTTP/3 & QUIC

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -6,11 +6,11 @@ for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow 
 ufw reload > /dev/null
 
 # OTHER EXAMPLE RULES
-# Restrict to port 80
+# Restrict to port 80 (tcp)
 #for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80 comment 'Cloudflare IP'; done
 
-# Restrict to port 443
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
+# Restrict to port 443 (tcp & udp)
+#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow from $cfip to any port 443 comment 'Cloudflare IP'; done
 
-# Restrict to ports 80 & 443
+# Restrict to ports 80 & 443 (tcp)
 #for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
